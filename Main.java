9bba2ff89be8ffc9b52b97dd62191dacdf3184a4
Janet Cho <98375156+janetscho@users.noreply.github.com>
2023-11-13 13:27:50 -0800

    forecast

--- Main.java
+++ Main.java
@@ -7,16 +7,18 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.json.*;
-import org.json.JSONObject;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	// please do not leak this key
+	// please do not leak this key i will kill you
 	public final static String map = "273a346184cebe767ac61195101d83ae";
 	
 	
@@ -32,25 +34,26 @@ public class Main extends Application {
     	
     	
     	//primaryStage.setScene(scene);
-    	primaryStage.show();
+    	//primaryStage.show();
     }
     
     // method to call API and store the resulting JSON data
     public static void APICall(String city) throws URISyntaxException, IOException {
-    	// toggles between imperial and metric data depending on the user
-    	//city = "Los Angeles";
-    	
+    	// imperial data
     	String apiURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + map + "&units=imperial";
     	String forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + map + "&units=imperial";
 
-    	//String apiURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + map + "&units=metric";
-
-    	URL url = new URL(apiURL);
+    	// metric data
+    	//String apiURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + map;
+    	//String forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + map;
     	
+    	// establishes connection with daily API
+    	URL url = new URL(apiURL);
     	URLConnection connection = url.openConnection();
     	BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
     	StringBuilder jsn = new StringBuilder();
     	
+    	// reading JSON data
     	String ln;
     	while((ln = rd.readLine()) != null)
     		jsn.append(ln);
@@ -66,30 +69,37 @@ public class Main extends Application {
     	int sunrise = data.getJSONObject("sys").getInt("sunrise");
     	int sunset = data.getJSONObject("sys").getInt("sunset");
     	
-    	
+    	// establishes connection with forecast API
     	url = new URL(forecastURL);
     	connection = url.openConnection();
     	rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
     	jsn = new StringBuilder();
     	
+    	// reading JSON data
     	while((ln = rd.readLine()) != null)
     		jsn.append(ln);
     	ln = "";
     	rd.close();
     	
-    	System.out.println(jsn.toString());
+    	// extracting forecast data
+    	JSONArray forecastData = new JSONObject(jsn.toString()).getJSONArray("list");
+    	Map<String, JSONObject> checkDate = new TreeMap<>();
+    	
+    	for(int i = 0; i < forecastData.length(); i++) {
+    		JSONObject day = forecastData.getJSONObject(i);
+    		String date = day.getString("dt_txt").split(" ")[0];
+    		
+    		if(!checkDate.containsKey(date))
+    			checkDate.put(date, day);
+    	}
+    	
+        for (Map.Entry<String, JSONObject> entry : checkDate.entrySet()) {
+            System.out.println("Date: " + entry.getKey());
+            System.out.println(entry.getValue());
+        }
     	
     	
     	
     }
-    
-    // converts Fahrenheit to Celsius
-    public double convertF(double degree) {
-    	return (degree - 32) * 5 / 9;
-    }
-    
-    // converts Celsius to Fahrenheit
-    public double convertC(double degree) {
-    	return (degree * 9 / 5) + 32;
-    }
+
 }
